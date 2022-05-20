a1014896025b8d58702910796b15d4a94dc3253f
AdrianLouis <adrian.louis.003@live.com>
2022-05-20 14:00:26 -0300

    listar e adicionar gases adicionado /controle

--- controle/controle.js
+++ controle/controle.js
@@ -146,7 +146,7 @@ async function listar() {
                     </div>
                 
                     <div class="undPrestRows flexCenter">
-                        <textarea rows=6' cols='40' id="textArea${elemento.id}">${elemento.anotacao}</textarea>
+                        <textarea rows=6' cols='40' id="textArea${elemento.id}">${(elemento.anotacao).trim()}</textarea>
                         <div class='btnEdit flexCenter'>
                             <i id='iconeEditar' onclick="editar(${elemento.id})" class="fas fa-pen"></i>
                             <i onclick='deletar(${elemento.id})' class="fas fa-trash"></i>
@@ -226,7 +226,7 @@ function novoRegistro() {
                     <input id='matricula' type='text' >
 
                     <label for='local'>Local</label>
-                    <input id='local' type='text' value='sala 209' >
+                    <input id='local' type='text' value='' >
 
                     <label for='funcao'>Função</label>
                     <input type='text' id='funcao' >
@@ -293,7 +293,7 @@ function salvar() {
                 matricula: String(matricula),
                 funcao: String(funcao),
                 funcionario: String(funcionario),
-                anotacao: String(anotacao)
+                anotacao: String((anotacao).trim())
             }
 
 
@@ -414,7 +414,7 @@ async function editar(id) {
         </div>
 
         <div class="undPrestRows flexCenter">
-            <textarea rows=6' cols='40' id="anotacao">${elemento.anotacao}</textarea>
+            <textarea rows=6' cols='40' id="anotacao">${(elemento.anotacao).trim()}</textarea>
 
             <div class='btnEdit flexCenter'>
                 <i onclick='atualizar(${id})' class="fas fa-save"></i>
@@ -466,7 +466,7 @@ function atualizar(id) {
                 matricula: String(matricula),
                 funcao: String(funcao),
                 funcionario: String(funcionario),
-                anotacao: String(anotacao)
+                anotacao: String((anotacao).trim())
             }
 
 
@@ -514,13 +514,13 @@ async function pesquisar() {
     container.innerHTML = ''
     let res = ''
 
-    
-    
+
+
     const response = await fetch('https://patiocgcontrole.azurewebsites.net/api/visitante')
     const jsonBody = await response.json()
-    
+
     jsonBody.forEach(elemento => {
- 
+
         // let itemBD = elemento.empresa
 
 
@@ -532,25 +532,25 @@ async function pesquisar() {
                 template(elemento)
             }
 
-        } else if (campoPesquisado == 'colaborador'){
+        } else if (campoPesquisado == 'colaborador') {
             let itemBD = elemento.colaborador
             itemBD = itemBD.toLowerCase();
 
             if (itemBD.includes(valor)) {
                 template(elemento)
             }
-        } else if (campoPesquisado == 'funcionario'){
+        } else if (campoPesquisado == 'funcionario') {
             let itemBD = elemento.funcionario
             itemBD = itemBD.toLowerCase();
 
-            if (itemBD.includes(valor)){
+            if (itemBD.includes(valor)) {
                 template(elemento)
             }
-        } else if (campoPesquisado == 'dia'){
+        } else if (campoPesquisado == 'dia') {
             let itemBD = elemento.dia
             itemBD = itemBD.toLowerCase();
 
-            if (itemBD.includes(valor)){
+            if (itemBD.includes(valor)) {
                 template(elemento)
             }
         }
@@ -559,25 +559,21 @@ async function pesquisar() {
 }
 
 
-
-
-
-
-function template(elemento){
+function template(elemento) {
     let res = ''
     let container = document.querySelector('#mainContainer')
 
     let dataDia = new Date(elemento.dia).getUTCDate()
-                dataDia < 10 ? dataDia = `0${dataDia}` : dataDia = dataDia
+    dataDia < 10 ? dataDia = `0${dataDia}` : dataDia = dataDia
 
-                let dataMes = new Date(elemento.dia).getUTCMonth() + 1
-                dataMes < 10 ? dataMes = `0${dataMes}` : dataMes = dataMes
+    let dataMes = new Date(elemento.dia).getUTCMonth() + 1
+    dataMes < 10 ? dataMes = `0${dataMes}` : dataMes = dataMes
 
-                let dataAno = new Date(elemento.dia).getUTCFullYear()
+    let dataAno = new Date(elemento.dia).getUTCFullYear()
 
-                let horario = elemento.hora.slice(0, 5)
+    let horario = elemento.hora.slice(0, 5)
 
-                res += `
+    res += `
         <div id='undPrest${elemento.id}' onclick="detalhesCard(${elemento.id}, this)" class="undPrestador flexCenter">
 
             <div class="undPrestRows flexCenter">
@@ -640,8 +636,188 @@ function template(elemento){
         </div>
             `
 
-                container.innerHTML += res
+    container.innerHTML += res
 
 
-    
+
 }
+
+
+
+
+// FUNCOES RELATIVAS AOS MEDIDORES DE GÁS
+
+function mainGas() {
+    let container = document.querySelector('#mainContainer')
+
+    //GET dos Registros
+
+    container.innerHTML = `
+    <div onclick='novoGas()' id="gasNovoReg" class="undPrestador flexCenter">
+        <span>Novo registro</span>
+    </div>
+
+    <div onclick='listarGas()' id="gasUltimosReg" class="undPrestador flexCenter">
+        <span>Mostrar registros</span>
+    </div>
+
+    <div id="gasEditarReg" class="undPrestador flexCenter">
+        <span>Editar registros</span>
+    </div>
+    `
+}
+
+async function listarGas() {
+
+    const response = await fetch('https://patiocgcontrole.azurewebsites.net/api/teste')
+    const jsonBody = await response.json()
+
+    let container = document.querySelector('#mainContainer')
+    container.innerHTML = "Carregando . . ."
+    res = ''
+
+    jsonBody.forEach(async elemento => {
+
+        let dia = elemento.dia
+        diaF = dia.slice(0, 10)
+        horaF = dia.slice(11)
+
+        res +=
+            `
+    <div class="undPrestador ">
+    <div class="gasesTh ">
+        <span>Data: ${diaF}</span>
+        <span>Hora: ${horaF}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 128</span>
+        <span>${elemento.lj128}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 132</span>
+        <span>${elemento.lj132}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 137</span>
+        <span>${elemento.lj137}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 152</span>
+        <span>${elemento.lj152}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 154</span>
+        <span>${elemento.lj154}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 157</span>
+        <span>${elemento.lj157}</span>
+    </div>
+    <div class="gases ">
+        <span>Loja 158</span>
+        <span>${elemento.lj158}</span>
+    </div>
+   
+</div>
+    `
+
+    })
+
+    container.innerHTML = res
+}
+
+function novoGas() {
+    let container = document.querySelector('#mainContainer')
+
+    container.innerHTML = `
+    <div class="undPrestador ">
+               
+        <div class="gases ">
+            <label for="lj128">Loja 128</label>
+            <input id="lj128" type="text">
+        </div>
+        <div class="gases ">
+            <label for="lj132">Loja 132</label>
+            <input type="text" id="lj132">
+        </div>
+        <div class="gases ">
+            <label for="lj137">Loja 137</label>
+            <input type="text" id="lj137">
+        </div>
+        <div class="gases ">
+            <label for="lj152">Loja 152</label>
+            <input type="text" id="lj152">
+        </div>
+        <div class="gases ">
+            <label for="lj154">Loja 154</label>
+            <input type="text" id="lj154">
+        </div>
+        <div class="gases ">
+            <label for="lj157">Loja 157</label>
+            <input type="text" id="lj157">
+        </div>
+        <div class="gases ">
+            <label for="lj158">Loja 158</label>
+            <input type="text" id="lj158">
+        </div>
+
+        <div class="gases">
+            <i class="fas fa-save" onclick='salvarGas()'></i>
+            <i onclick='mainGas()' class="fas fa-undo"></i>
+        </div>
+               
+    </div>
+    `
+}
+
+function salvarGas() {
+
+    let data = new Date()
+    let dataFormatada = data.toLocaleDateString('pt-Br', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    })
+
+    let lj128 = document.querySelector('#lj128').value
+    let lj132 = document.querySelector('#lj132').value
+    let lj137 = document.querySelector('#lj137').value
+    let lj152 = document.querySelector('#lj152').value
+    let lj154 = document.querySelector('#lj154').value
+    let lj157 = document.querySelector('#lj157').value
+    let lj158 = document.querySelector('#lj158').value
+
+    if (lj128 == '' || lj132 == '' || lj137 == '' || lj152 == '' || lj154 == '' || lj157 == '' || lj158 == '') {
+        alert('Com exceção do campo de Anotações, não pode haver campos em branco.')
+    } else {
+
+        async function postando() {
+
+            const obj = {
+                dia: String(dataFormatada),
+                lj128: String(lj128).trim(),
+                lj132: String(lj132).trim(),
+                lj137: String(lj137).trim(),
+                lj152: String(lj152).trim(),
+                lj154: String(lj154).trim(),
+                lj157: String(lj157).trim(),
+                lj158: String(lj158).trim()
+            }
+
+            const response = await fetch("https://patiocgcontrole.azurewebsites.net/api/teste/", {
+                method: "POST",
+                body: JSON.stringify(obj),
+                headers: { 'Content-Type': 'application/json' }
+            })
+        }
+
+        postando()
+
+        setTimeout(() => {
+            listarGas()
+        }, 1000);
+    }
+}
+
