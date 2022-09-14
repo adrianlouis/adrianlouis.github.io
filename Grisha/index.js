9e58c0066d6a74f9a8997ab061aae6424848453b
AdrianLouis <adrian.louis.003@live.com>
2022-09-14 01:47:13 -0300

    Responsividade aplicada em 650px

--- Grisha/index.js
+++ Grisha/index.js
@@ -1,7 +1,10 @@
 const target = document.querySelectorAll('[data-anime]')
 const animationClass = 'animate'
+const imagens = document.querySelectorAll('.animeUp')
 
 function animeScroll(){
+    console.log(imagens)
+
     const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4)
     target.forEach((elem)=>{
         if((windowTop) > elem.offsetTop){
@@ -10,11 +13,20 @@ function animeScroll(){
             elem.classList.remove('animate')
         }
     })
+
+    const larguraTela = window.screen.width 
+    if (larguraTela <= 390){
+        imagens.forEach((imgs)=>{
+            imgs.setAttribute('data-anime', 'up')
+        })
+    }
+    
+
 }
 
 if(target.length){
-    console.log(target)
     window.addEventListener('scroll', ()=>{
         animeScroll();
     })
 }
+
